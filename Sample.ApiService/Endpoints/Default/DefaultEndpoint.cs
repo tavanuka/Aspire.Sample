adba8cb9afd81b3b24ec4ea79d83fb11bc98504e
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2025-12-12 23:24:37 +0100

    fix(backend): default endpoint is not mapped

---
+++ Sample.ApiService/Endpoints/Default/DefaultEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+
+namespace Sample.ApiService.Endpoints.Default;
+
+public class DefaultEndpoint : Ep.NoReq.Res<string>
+{
+    public override void Configure()
+    {
+        Get("/");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct) 
+        => await Send.OkAsync("API service is running. Navigate to /weatherforecast to see sample data.", ct);
+}
